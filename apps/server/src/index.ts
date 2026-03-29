068c6625e7bd52458193be2ce8a9cc6d78a9f49e
mantrakp04 <mantrakp@gmail.com>
2026-03-28 20:06:16 -0700

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,27 @@
+import { cors } from "@elysiajs/cors";
+import { createContext } from "@g-spot/api/context";
+import { appRouter } from "@g-spot/api/routers/index";
+import { env } from "@g-spot/env/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST", "OPTIONS"],
+    }),
+  )
+  .all("/trpc/*", async (context) => {
+    const res = await fetchRequestHandler({
+      endpoint: "/trpc",
+      router: appRouter,
+      req: context.request,
+      createContext: () => createContext({ context }),
+    });
+    return res;
+  })
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
